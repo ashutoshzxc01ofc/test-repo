4d2847060472c5439ea2616037708e5b3afc9567
ashutoshverma02 <ashutosh.verma02@nagarro.com>
2024-10-15 17:06:58 +0530

    initial commmit

---
+++ src/App.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import "./App.css";
+import "./styletoken.css";
+
+function App() {
+  const [hover, setHover] = useState(false);
+  return (
+    <div
+      className="app-layout"
+      style={{
+        backgroundColor: "var(--colors-primary-1)",
+      }}
+    >
+      <div
+        className="m-2 h-16 flex items-center rounded-md"
+        style={{
+          backgroundColor: "var(--colors-primary-2)",
+        }}
+      >
+        <div className="text-white font-normal font-mono text-3xl ml-4">
+          Finch Plugin
+        </div>
+      </div>
+      <div
+        style={{
+          backgroundColor: "var(--colors-primary-1)",
+        }}
+        className="h-full"
+      >
+        <button
+          style={{
+            borderRadius: "var(--corner-radius-xxl)",
+            padding: "var(--spacing-xl)",
+            color: "var(--button-text)",
+            backgroundColor: hover
+              ? "var(--interactive-hover)"
+              : "var(--interactive-default)",
+          }}
+          onMouseEnter={() => setHover(true)}
+          onMouseLeave={() => setHover(false)}
+        >
+          FINCH POC
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
