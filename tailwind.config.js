a4097f2a4463136cfd0573323bf2542a284fbb1d
ashutoshzxc01ofc <144043196+ashutoshzxc01ofc@users.noreply.github.com>
2024-11-13 11:18:17 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+import { designTokens } from "./src/designTokens"; // Import the design tokens
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
         mulish: ["Mulish"],
+      },
+
+      colors: {
+        ...designTokens.colors,
+      },
+
+      fontSize: {
+        ...designTokens.fontSize,
+      },
+
+      spacing: {
+        ...designTokens.spacing,
       },
     },
   },
