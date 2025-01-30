4b9a99a6bcf5d51f1493b1a777431b08606eac9b
ashutoshzxc01ofc <144043196+ashutoshzxc01ofc@users.noreply.github.com>
2025-01-30 16:23:09 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,26 +10,28 @@ import anaomalyicon from "../public/Anaomaly.svg";
 import Card from "./Card";
 import frameuser from "../public/Frame_useracc.png";
 import logo from "../public/Logo.png";
+import pattern from "../public/Pattern.png";
 
 
 function App() {
   return (
-    <div className="app-layout bg-dark-purple-100">
-      <div className=" h-[68px] flex items-center justify-between bg-dark-purple-100 shadow-[0px_4px_120px_#603fa966]">
+    <div className="app-layout bg-[#1a1628]">
+      <div className=" h-[40px] flex z-10 items-center justify-between bg-[#1a1628] shadow-[0px_4px_120px_#603fa966]">
         <div className="text-white font-medium text-3xl ">
-          <img src={logo} alt="Logo" className="h-10 ml-2" />
+          <img src={logo} alt="Logo" className="h-8 ml-3 mt-2" />
         </div>
         <div>
-          <img src={frameuser} alt="user" className="h-10 mr-14" />
+          <img src={frameuser} alt="user" className="h-8 mr-12" />
         </div>
       </div>
 
       <div className="flex w-full h-full">
-        <div className="w-16 bg-dark-purple-100 " />
-        <div className="w-11/12 h-screen overflow-auto ">
-        <div className="text-white text-2xl font-bold ml-6 mt-4">Welcome to RBQM 360</div>
-        <div className="text-white text-lg font-normal ml-6 mt-1">Check out our latest services</div>
-          <div className="h-8 w-full"></div>
+        <img src={pattern} className="absolute right-14 h-72 w-[25%] top-6"/>
+        <div className="w-16  bg-[#1a1628]" />
+        <div className="w-9/12 h-screen ml-10">
+        <div className="text-white text-base font-bold ml-14 mt-4">Welcome to RBQM 360</div>
+        <div className="text-white text-xs font-normal ml-14 mt-1">Check out our latest services</div>
+          <div className="h-4 w-full"></div>
           <div className=" flex justify-evenly mr-32 w-full">
             <Card 
             text_content="Identify, assess, and manage risks associated with CtQ factors
@@ -56,7 +58,7 @@ function App() {
             icon={portfolio} />
             
           </div>
-          <div className="h-8 w-full"></div>
+          <div className="h-4 w-full"></div>
           <div className="flex justify-evenly mr-32 w-full">
           <Card 
             text_content="Quality events monitoring solution to review and monitor GCP
