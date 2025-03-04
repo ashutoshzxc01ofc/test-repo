68226e02a073043350c81df438b26fda0fe0a285
ashutoshzxc01ofc <144043196+ashutoshzxc01ofc@users.noreply.github.com>
2025-03-04 18:42:59 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,86 +1,181 @@
 import "./App.css";
-import "./styletoken.css";
-// import Logo from "../public/Finch Logo.svg";
-import RACT from "../public/RACT.svg";
-import RACT_EFF from "../public/RACT_eff.svg";
-import study from "../public/Study.svg";
-import portfolio from "../public/Portfolio.svg";
-import issueicon from "../public/Issue.svg";
-import anaomalyicon from "../public/Anaomaly.svg";
-import Card from "./Card";
-import frameuser from "../public/Frame_useracc.png";
-import logo from "../public/Logo.png";
-import pattern from "../public/Pattern.png";
 
+import Logo from "../public/Finch Logo.svg";
+import microsoftLogo from "../src/assets/microsoft.svg.png";
 
 function App() {
+  //const [hover, setHover] = useState(false);
+
   return (
-    <div className="app-layout bg-dark-purple-100 h-full">
-      <div className=" h-[64px] flex z-10 items-center justify-between bg-dark-purple-100 shadow-[0px_4px_120px_#603fa966]">
-        <div className="text-white font-medium text-3xl ">
-          <img src={logo} alt="Logo" className="h-10 ml-3 mt-2" />
-        </div>
-        <div>
-          <img src={frameuser} alt="user" className="h-10 mr-12" />
+    <div className="app-layout">
+      <div
+        className=" h-14 flex justify-between items-center bg-brand-primary-p1-main-light-mode"
+        // style={{
+        //   backgroundColor: "var(--colors-brands-primary-p1-main)",
+        // }}
+      >
+        <div className="text-white font-medium text-3xl ml-32 ">
+          {" "}
+          <img src={Logo} alt="Logo" className="h-10" />
         </div>
       </div>
-
-      <div className="flex w-full h-full">
-        <img src={pattern} className="absolute right-2 top-8"/>
-        <div className="w-16  bg-dark-purple-100" />
-        <div className="w-full pr-28 h-screen overflow-y-auto ml-10 mb-8">
-          <div className="text-white text-2xl font-bold ml-6 mt-10">Welcome to RBQM 360</div>
-          <div className="text-white text-base font-normal ml-6 mt-2 mb-6">Check out our latest services</div>
-          <div className="h-4 w-full"></div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-            <Card 
-            text_content="Identify, assess, and manage risks associated with CtQ factors
-                  whose integrity is fundamental to the protection of study
-                  participants, the reliability and interpretability of the
-                  study results, and the decisions made based on the study
-                  results."
-            text_heading="RACT"
-            icon={RACT} />
-
-            <Card 
-            text_content="Study level Risk-based Quality Management (RBQM) solution used
-                  to monitor KRI/QTL performance, identify and manage risk
-                  signals/issues to ensure that these risks are addressed before
-                  they become serious issues."
-            text_heading="Study Insights"
-            icon={study} />
-
-            <Card 
-            text_content=" GCP quality oversight solution that empowers users to oversee
-                  health of portfolio, identify portfolio/process/vendor/study
-                  risks and communicate quality management activities."
-            text_heading="Portfolio Insights"
-            icon={portfolio} />
-            
-          {/* </div>
-          <div className="h-4 w-full"></div>
-          <div className="flex justify-evenly w-full mb-24"> */}
-          <Card 
-            text_content="Quality events monitoring solution to review and monitor GCP
-                  quality events and issues at portfolio/study level."
-            text_heading="Issue Insights" 
-            icon={issueicon}/>
-
-          <Card 
-            text_content="Provides key insights to the RBQM CoE to measure, track and
-                  visualize the adoption and impact of RBQM across the portfolio."
-            text_heading="RBQM Effectiveness." 
-            icon={RACT_EFF}/>
-
-          <Card 
-            text_content="Identify, analyze, review and collaborate to eliminate
-                  anomalies in clinical data using Advanced Analytics , AI/ ML
-                  with active learning engine to expedite clinical data
-                  management and data review."
-            text_heading="Anomaly Detection"
-            icon={anaomalyicon} />
+      <div className="h-full flex">
+        <div
+          className="w-1/2 flex items-center justify-center shadow bg-grey-light-light-mode"
+          // style={{
+          //   backgroundColor: "var(--colors-grey-light)",
+          // }}
+        >
+          <div
+            className="w-8/12 p-4 bg-white text-miscellaneous-miscellaneous-text-3-light-mode rounded-small-default"
+            style={
+              {
+                // borderRadius: "var(--corner-radius-s)",
+                // color: "var(--colors-miscellaneous-miscellaneous-text-3)",
+                //  boxShadow: `0 4px 6px -1px var(--colors-shadow-shadow-4), 0 2px 4px -1px var(--colors-shadow-shadow-4)`,
+                //  boxShadow: `0 0 10px 2px  var(--colors-shadow-shadow-4)`,
+                //boxShadow: ` var(--number-6)    var(--number-6)    var(--number-6)   var(--number-6)  var(--colors-shadow-shadow-4)`,
+              }
+            }
+          >
+            <div className="font-mulish font-black text-5xl">
+              Finch Design System
+            </div>
+            <div className="font-medium text-lg mt-2">
+              A Robust & Automated Framework for Scalable Design
+            </div>
+            <div className="text-sm mt-8 font-normal ">
+              Finch Design System is a robust framework designed to streamline
+              design and development through reusable components and automated
+              workflows. It ensures consistency, accessibility, and scalability
+              across platforms while enabling seamless collaboration. Finch
+              empowers teams to focus on innovation with efficiency and
+              flexibility at its core.
+            </div>
           </div>
-          <div className="h-10 mb-16 w-full"></div>
+        </div>
+        <div className="  flex items-center justify-center  w-1/2">
+          <form className=" space-y-4  px-20 w-10/12 ">
+            <div
+              className="text-3xl font-semibold text-miscellaneous-miscellaneous-text-3-light-mode 
+"
+              // style={{
+              //   color: "var(--colors-miscellaneous-miscellaneous-text-3)",
+              // }}
+            >
+              Log in into your account!
+            </div>
+            <div
+              className="text-sm  pb-4 font-medium cursor-pointer  underline underline-offset-2  text-miscellaneous-hyperlink-light-mode
+"
+              // style={{
+              //   color: "var(--colors-miscellaneous-hyperlink)",
+              // }}
+            >
+              How to get username and password?
+            </div>
+            <div>
+              <div
+                className="mb-2 text-base text-miscellaneous-miscellaneous-text-3-light-mode"
+                // style={{
+                //   color: "var(--colors-miscellaneous-miscellaneous-text-3)",
+                // }}
+              >
+                Email
+              </div>
+              <label>
+                <input
+                  type="email"
+                  name="email"
+                  required
+                  className="border w-full  p-2 bg-surface-surface border-surface-line ext-type-secondary-light-mode rounded-xsmall-default"
+                  style={
+                    {
+                      // backgroundColor: "var(--colors-surface-surface)",
+                      //borderRadius: "var(--corner-radius-xs)",
+                      // padding: "var(--spacing-md)",
+                      // borderColor: "var( --colors-surface-line)",
+                      // color: "var(--colors-type-secondary)",
+                    }
+                  }
+                />
+              </label>
+            </div>
+            <div>
+              <div
+                className="mb-2 text-base text-miscellaneous-miscellaneous-text-3-light-mode"
+                // style={{
+                //   color: "var(--colors-miscellaneous-miscellaneous-text-3)",
+                // }}
+              >
+                Password
+              </div>
+              <label>
+                <input
+                  type="password"
+                  name="phone"
+                  required
+                  className="border p-2 w-full bg-surface-surface border-surface-line text-type-secondary-light-mode rounded-xsmall-default"
+                  style={
+                    {
+                      //  backgroundColor: "var(--colors-surface-surface)",
+                      //   borderRadius: "var(--corner-radius-xs)",
+                      // padding: "var(--spacing-md)",
+                      // borderColor: "var( --colors-surface-line)",
+                      // color: "var(--colors-type-secondary)",
+                    }
+                  }
+                />
+              </label>
+            </div>
+            <div>
+              <button
+                type="submit"
+                className="font-normal mt-8 w-full text-miscellaneous-miscellaneous-text-2-light-mode bg-interactive-default-light-mode hover:bg-interactive-hover-light-mode rounded-xxlarge-default py-xs-cozy"
+                // style={{
+                //   // borderRadius: "var(--corner-radius-xxl)",
+                //   padding: "var(--spacing-md) var(--spacing-lg)",
+                //   //  color: "var(--colors-miscellaneous-miscellaneous-text-2)",
+                //   // backgroundColor: hover
+                //   //   ? "var(--colors-interactive-hover)"
+                //   //   : "var(--colors-interactive-default)",
+                // }}
+                // onMouseEnter={() => setHover(true)}
+                // onMouseLeave={() => setHover(false)}
+              >
+                LogIn
+              </button>
+              <div className="w-full flex justify-center text-xs text-slate-500 my-6">
+                Or Log In with
+              </div>
+              <button
+                type="submit"
+                className="font-base flex justify-between border w-full text-black  border-interactive-default-light-mode rounded-xxlarge-default "
+                //  className="font-base flex justify-between border w-full text-black border-interactive-default rounded-xxlarge p-md"
+                style={{
+                  // borderRadius: "var(--corner-radius-xxl)",
+                  padding: "var(--spacing-md) ",
+                  // padding: "var(--spacing-md) ",
+                  //   borderColor: "var(--colors-interactive-default)",
+                  // color: "var(--button-text)",
+                }}
+                // onMouseEnter={() => setHover(true)}
+                // onMouseLeave={() => setHover(false)}
+              >
+                <img src={microsoftLogo} alt="Logo" className="h-6 ml-2" />
+                <div
+                  className="text-sm font-medium -ml-5  text-interactive-default-light-mode py-xs-cozy"
+                  // style={{
+                  //   color: "var(--colors-interactive-default)",
+                  //   padding: "var(--spacing-sm) var(--spacing-md)",
+                  // }}
+                >
+                  LogIn with Microsoft
+                </div>
+                <div></div>
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
