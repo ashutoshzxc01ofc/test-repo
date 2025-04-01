1a639af5df60d4b1d33c5761b724e5c322f70018
ashutoshzxc01ofc <144043196+ashutoshzxc01ofc@users.noreply.github.com>
2025-04-01 08:49:23 +0530

    committed

--- src/test.js
+++ src/test.js
@@ -2,6 +2,7 @@ export const designTokens = {
   "colors": {
     "primary": "#3498db",
     "secondary": "#2ecc71",
+    "ashutosh": "#6473883",
     "hamdur": "#54o534592",
     "nitish": "#fakl5435"
   },
